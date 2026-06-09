16e20f7894c4b5763337d67bd31f44d2111e93a3
zitaofeng988 <zitaofeng988@gmail.com>
2026-06-09 13:11:45 +0800

    refactor: 优化消息文件类型判断

--- src/operation/components/MessageDialog/utils.ts
+++ src/operation/components/MessageDialog/utils.ts
@@ -2,9 +2,21 @@ import type { UploadUserFile } from 'element-plus'
 
 export type MessageFileType = 'image' | 'video'
 
+const getFileMimeType = (file: File | UploadUserFile) => {
+  if ('type' in file && typeof file.type === 'string') {
+    return file.type
+  }
+
+  if ('raw' in file) {
+    return file.raw?.type || ''
+  }
+
+  return ''
+}
+
 export const getMessageFileType = (file: File | UploadUserFile): MessageFileType => {
   const fileName = file.name || ''
-  const fileType = (file as File).type || (file as UploadUserFile).raw?.type || ''
+  const fileType = getFileMimeType(file)
 
   if (fileType.startsWith('video/') || /\.(mp4|avi|mov|wmv|flv|mkv)$/i.test(fileName)) {
     return 'video'
