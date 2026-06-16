ff0da09627ff2cf0d7d0dbce41ec12beab8f26b8
zitaofeng988 <zitaofeng988@gmail.com>
2026-06-16 16:08:25 +0800

    feat: 添加按时间销售报表汇总接口

--- src/api/opertion/DataStatistics/SaleByTimeReport/index.ts
+++ src/api/opertion/DataStatistics/SaleByTimeReport/index.ts
@@ -1,1 +1,13 @@
-export {}
+import request from '@/axios'
+import type { SaleByTimeReportData, SaleByTimeReportParams } from './types'
+
+export * from './types'
+
+export const getSaleByTimeReport = (
+  params?: SaleByTimeReportParams
+): Promise<IResponse<SaleByTimeReportData>> => {
+  return request.get<SaleByTimeReportData>({
+    url: '/v2/system/stats/summary',
+    params
+  })
+}
--- src/api/opertion/DataStatistics/SaleByTimeReport/types.ts
+++ src/api/opertion/DataStatistics/SaleByTimeReport/types.ts
@@ -1,1 +1,23 @@
-export {}
+export interface SaleByTimeReportParams {
+  start_time?: string
+  end_time?: string
+  keyword?: string
+  order?: string
+}
+
+export interface SaleByTimeReportSummaryItem {
+  batch_active?: number | string
+  batch_energy?: number | string
+  flash_energy?: number | string
+  hosting?: number | string
+  level?: number | string
+  stroke_energy?: number | string
+  time_energy?: number | string
+  total?: number | string
+  weal_energy?: number | string
+}
+
+export interface SaleByTimeReportData {
+  energy?: SaleByTimeReportSummaryItem[]
+  order?: SaleByTimeReportSummaryItem[]
+}
