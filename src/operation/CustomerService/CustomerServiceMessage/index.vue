04f512c8941094bf34c30bf92032bf727fa1d5d5
zitaofeng988 <zitaofeng988@gmail.com>
2026-08-12 18:49:38 +0800

    refactor: 简化客服消息页面容器

--- src/operation/CustomerService/CustomerServiceMessage/index.vue
+++ src/operation/CustomerService/CustomerServiceMessage/index.vue
@@ -1,10 +1,7 @@
 <template>
-  <ContentWrap>
-    <CustomerServiceChat />
-  </ContentWrap>
+  <CustomerServiceChat />
 </template>
 
 <script setup lang="ts">
-import { ContentWrap } from '@/components/ContentWrap'
 import CustomerServiceChat from './components/CustomerServiceChat/index.vue'
 </script>
