5f416e4230c3d74f808dd9e33fcdef3e6850f58d
zitaofeng988 <zitaofeng988@gmail.com>
2026-08-17 16:46:52 +0800

    refactor: 个人中心移除谷歌验证码页签

--- src/views/Personal/PersonalCenter/PersonalCenter.vue
+++ src/views/Personal/PersonalCenter/PersonalCenter.vue
@@ -7,7 +7,6 @@ import UploadAvatar from './components/UploadAvatar.vue'
 import { Dialog } from '@/components/Dialog'
 import EditInfo from './components/EditInfo.vue'
 import EditPassword from './components/EditPassword.vue'
-import GoogleAuthenticator from './components/GoogleAuthenticator.vue'
 import { isManagementSystem } from '@/utils/system'
 
 const isManagement = isManagementSystem()
@@ -107,9 +106,6 @@ const saveAvatar = async () => {
         </ElTabPane>
         <ElTabPane label="修改密码" name="second">
           <EditPassword />
-        </ElTabPane>
-        <ElTabPane v-if="!isManagement" label="谷歌验证码" name="google-authenticator">
-          <GoogleAuthenticator />
         </ElTabPane>
       </ElTabs>
     </ContentWrap>
